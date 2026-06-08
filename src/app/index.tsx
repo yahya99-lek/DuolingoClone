4b4108204b3c04a7e09fce1eb860c17abfe65408
yahya-lek99 <yahya.lek99@gmail.com>
2026-06-08 20:45:26 +0400

    implement the onboarding screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,20 @@
-import { Text, View } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Stack, router } from "expo-router";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="h1 mt-5 color-lingua-blue">
-        Edit test. no way
-      </Text>
-    </View>
+    <>
+      <Stack.Screen options={{ headerShown: false }} />
+      <View className="flex-1 justify-center items-center bg-white px-6 gap-4">
+        <Text className="h1 text-center">Welcome to Duolingo</Text>
+        <TouchableOpacity
+          className="btn-primary w-full"
+          onPress={() => router.push("/onboarding")}
+          activeOpacity={0.85}
+        >
+          <Text className="btn-text">View Onboarding</Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
